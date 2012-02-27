db7fb1d679ccf04a7024512fd5425cc42cbd6573
Micah Woods <micahwoods@gmail.com>
2012-02-27 10:08:16 -0600

    Passed the word count test

--- hm1/strings.rb
+++ hm1/strings.rb
@@ -2,3 +2,10 @@ def palindrome? string
   string = string.downcase.gsub(/\W/,'')
   string.reverse == string
 end
+
+def count_words string
+  keys = string.downcase.scan(/\w+/)
+  counts = Hash.new(0)
+  keys.each { |key| counts[key] += 1 }
+  counts
+end
--- hm1/test_strings.rb
+++ hm1/test_strings.rb
@@ -8,4 +8,9 @@ class TestStrings < Test::Unit::TestCase
     assert palindrome?("madam, I'm Adam!"), "Should be a palindrome"
     assert !palindrome?('Abracadabra'), "NOT a palindrome"
   end
+
+  def test_count_words
+    assert_equal count_words("A man, a plan, a canal -- Panama"), {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
+    assert_equal count_words("Doo bee doo bee doo"), {'doo' => 3, 'bee' => 2}
+  end
 end
