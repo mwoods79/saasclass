4c118c4c881f9c1a803063f6f50c5871f2a16378
Micah Woods <micahwoods@gmail.com>
2012-03-02 14:01:41 -0600

    First Part Of Rock Paper Scissors is DONE!

--- hm1/rock_paper_scissors.rb
+++ hm1/rock_paper_scissors.rb
@@ -3,5 +3,13 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
-
+  player1 = game.first
+  player2 = game.last 
+  play1  =  player1.last.downcase 
+  play2  =  player2.last.downcase
+  raise NoSuchStrategyError unless play1.match(/[p|r|s]/) and play2.match(/[p|r|s]/)
+  return player2 if play1 == 'r' and play2 == 'p'
+  return player2 if play1 == 's' and play2 == 'r'
+  return player2 if play1 == 'p' and play2 == 's'
+  player1
 end
--- hm1/test_rock_paper_scissors.rb
+++ hm1/test_rock_paper_scissors.rb
@@ -15,8 +15,21 @@ class TestRockPaperScissors < Test::Unit::TestCase
     end
   end
 
-  #def test_returns_the_winner
-    #assert_equal rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ]), [ "Dave", "S" ]
-  #end
+  def test_cant_use_dynomite_or_nukes
+    assert_raise(NoSuchStrategyError) do
+      rps_game_winner([ [ "Armando", "P" ], [ "Dave", "N" ] ])
+    end
+  end
 
+  def test_scissors_beat_paper
+    assert_equal rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ]), [ "Dave", "S" ]
+  end
+
+  def test_paper_beats_rock
+    assert_equal rps_game_winner([ [ "Armando", "R" ], [ "Dave", "P" ] ]), [ "Dave", "P" ]
+  end
+
+  def test_rock_beats_scissors
+    assert_equal rps_game_winner([ [ "Armando", "S" ], [ "Dave", "R" ] ]), [ "Dave", "R" ]
+  end
 end
