23045531b61af57257cf3ab778f73017dbac0e29
Micah Woods <micahwoods@gmail.com>
2012-03-04 12:46:36 -0600

    Rock paper scissors finished

--- hm1/rock_paper_scissors.rb
+++ hm1/rock_paper_scissors.rb
@@ -1,7 +1,7 @@
 class WrongNumberOfPlayersError < StandardError ; end 
 class NoSuchStrategyError < StandardError ; end
 
-def rps_game_winner(game)
+def rps_game_winner game 
   raise WrongNumberOfPlayersError unless game.length == 2
   player1 = game.first
   player2 = game.last 
@@ -12,4 +12,15 @@ def rps_game_winner(game)
   return player2 if play1 == 's' and play2 == 'r'
   return player2 if play1 == 'p' and play2 == 's'
   player1
+end
+
+def rps_tournament_winner tournament
+  # if the tournament is only one player, return the player
+  return tournament if tournament.flatten.length == 2
+  # mini tournaments
+  first = tournament.first
+  second = tournament.last
+
+  # return the game winner from the recursive calls
+  rps_game_winner([rps_tournament_winner(first), rps_tournament_winner(second)])
 end
--- hm1/test_rock_paper_scissors.rb
+++ hm1/test_rock_paper_scissors.rb
@@ -32,4 +32,51 @@ class TestRockPaperScissors < Test::Unit::TestCase
   def test_rock_beats_scissors
     assert_equal rps_game_winner([ [ "Armando", "S" ], [ "Dave", "R" ] ]), [ "Dave", "R" ]
   end
+
+  def test_rps_game_winner_is_case_insensitive
+    assert_equal rps_game_winner([ [ "Armando", "s" ], [ "Dave", "r" ] ]), [ "Dave", "r" ]
+  end
+
+  def test_first_player_wins_if_tied
+    assert_equal rps_game_winner([ [ "Armando", "r" ], [ "Dave", "r" ] ]), [ "Armando", "r" ]
+  end
+
+
+  def test_integration_there_can_be_a_tournament
+    assert_equal rps_tournament_winner(
+    [
+      [
+        [
+          ["Armando", "P"], ["Dave", "S"]
+        ],
+        [
+          ["Richard", "R"], ["Michael", "S"] ]
+      ],
+      [
+        [
+          ["Allen", "S"], ["Omer", "P"]
+        ],
+        [
+          ["David E.", "R"], ["Richard X.", "P"]
+        ]
+      ]
+    ]), ["Richard", "R"]
+  end
+
+  def test_tournament_returns_a_winner_for_simple_tournament
+    assert_equal rps_tournament_winner(
+      [
+        [
+          ["Armando", "P"], ["Dave", "S"]
+        ],
+        [
+          ["Richard", "R"], ["Michael", "S"]
+        ]
+      ]
+    ), [ "Richard", "R" ]
+  end
+
+  def test_tournament_returns_a_winner_for_a_more_complicated_tournament
+    assert_equal rps_tournament_winner([ [ "Armando", "S" ], [ "Dave", "R" ] ]), [ "Dave", "R" ]
+  end
 end
